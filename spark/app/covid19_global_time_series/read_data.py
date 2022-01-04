2119db89591bf5413b91789b916942e93ddde96c
Katawut-p <Katawut.p@outlook.com>
2022-01-04 15:06:39 +0700

    fix datetime reformat error

--- spark/app/covid19_global_time_series/read_data.py
+++ spark/app/covid19_global_time_series/read_data.py
@@ -1,5 +1,7 @@
 import sys
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as f
+from pyspark.sql.types import TimestampType
+from datetime import datetime
 from modules import reformat_date
 
 # Create spark session
@@ -40,9 +42,25 @@ print("######################################")
 print("Change data type")
 print("######################################")
 
+def re_format_date(date):
+        # yyyy-MM-dd HH:mm:ss  <- format example 11/22/21 -> 2021-11-22 00:00:00
+        x = date.split('/')
+        year = '20' + x[2]
+        month = x[0] if int(x[0]) > 9 else '0' + x[0]
+        day = x[1] if int(x[1]) > 9 else '0' + x[1]
+    
+        #Concat date and convert to dateTime format
+        timestamp_str = "{}-{}-{}".format(year, month, day)
+        new_timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d')
+    
+        return new_timestamp
+
+re_format_date_udf = f.udf(lambda date: re_format_date(date), TimestampType())
+
 final_df = covid19_global_df.withColumn('Confirmed', covid19_global_df['Confirmed'].cast('int')) \
                  .withColumn('Death', covid19_global_df['Death'].cast('int')) \
-                 .withColumn('Date', reformat_date.PythonFunctions.re_format_date(covid19_global_df['Date']))
+                 .withColumn('Date', re_format_date_udf(covid19_global_df['Date'])
+                 )
 
 ####################################
 # Export covid19 global data to csv file
